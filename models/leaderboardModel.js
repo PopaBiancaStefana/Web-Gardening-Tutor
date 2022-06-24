3e6ebb10637b59ac50ff3c56192d3aff68c90921
GODOVANIUC Victoria <victoria.godovaniuc@info.uaic.ro>
2022-06-24 17:25:40 +0300

    Deleted photo_path field from registered_users, because we are not using it and Leaderboard gets photos

--- models/leaderboardModel.js
+++ models/leaderboardModel.js
@@ -14,7 +14,7 @@ async function topUsers() {
 
 async function topUsersTable() {
     return new Promise((resolve, reject) => {
-        db.pool.query("select * from (select r.profile_information , r.name,  ROW_NUMBER() over (order by count(c.finished) desc) rownum, count(c.finished) as 'finished_courses' from registered_users r join courses_in_progress c on r.id = c.id_user and c.finished = 1 group by r.name, c.finished, r.photo_path) as q where rownum<=10;", [], (err, result) => {
+        db.pool.query("select * from (select r.profile_information , r.name,  ROW_NUMBER() over (order by count(c.finished) desc) rownum, count(c.finished) as 'finished_courses' from registered_users r join courses_in_progress c on r.id = c.id_user and c.finished = 1 group by r.name, c.finished, r.profile_information) as q where rownum<=10;", [], (err, result) => {
             if (err) {
                 console.log(err);
                 reject(err);
@@ -27,16 +27,9 @@ async function topUsersTable() {
                 let row = result[key];
                 let user = {};
 
-                file = path.join(__dirname, "../public/users", row.profile_information);
+                let file = path.join(__dirname, "../public/users", row.profile_information);
                 profileDetails = require(file);
                 user.photo_path = profileDetails = profileDetails.photo_path;
-
-                // if (row.photo_path == null) {
-                //     user.photo_path = "";
-                // }
-                // else {
-                //     user.photo_path = row.photo_path;
-                // }
                 user.name = row.name;
                 user.rank = row.rownum;
                 user.finished_courses = row.finished_courses;
