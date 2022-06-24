01815eb78a02df3061cfbc95029b8d43b3589b74
GODOVANIUC Victoria <victoria.godovaniuc@info.uaic.ro>
2022-06-24 16:30:05 +0300

    Tried to add avatars on leaderboard, but I got an error

--- models/leaderboardModel.js
+++ models/leaderboardModel.js
@@ -1,4 +1,5 @@
 const db = require("../database");
+const path = require("path");
 
 
 async function topUsers() {
@@ -13,7 +14,7 @@ async function topUsers() {
 
 async function topUsersTable() {
     return new Promise((resolve, reject) => {
-        db.pool.query("select * from (select r.photo_path, r.name,  ROW_NUMBER() over (order by count(c.finished) desc) rownum, count(c.finished) as 'finished_courses' from registered_users r join courses_in_progress c on r.id = c.id_user and c.finished = 1 group by r.name, c.finished, r.photo_path) as q where rownum<=10;", [], (err, result) => {
+        db.pool.query("select * from (select profile_information, r.name,  ROW_NUMBER() over (order by count(c.finished) desc) rownum, count(c.finished) as 'finished_courses' from registered_users r join courses_in_progress c on r.id = c.id_user and c.finished = 1 group by r.name, c.finished, profile_information) as q where rownum<=10;", [], (err, result) => {
             if (err) {
                 console.log(err);
                 reject(err);
@@ -25,12 +26,11 @@ async function topUsersTable() {
             Object.keys(result).forEach((key) => {
                 let row = result[key];
                 let user = {};
-                if (row.photo_path == null) {
-                    user.photo_path = "";
-                }
-                else {
-                    user.photo_path = row.photo_path;
-                }
+                let fileName = row.profile_information;
+                let filePath = path.join(__dirname, "../public/users", fileName);
+                let profileInfo = require(filePath);
+
+                user.photo_path = profileInfo = profileInfo.photo_path;
                 user.name = row.name;
                 user.rank = row.rownum;
                 user.finished_courses = row.finished_courses;
